98fb26f520404d8cfa4b875ab9a03a86d01ead77
Deepakshi275 <sharmapanchi99@gmail.com>
2024-05-02 17:28:07 +0530

    first commit

---
+++ counter/src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import Counter from './Counter';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ counter/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="title">Counter App</h1>
+        <p className="counter">{this.state.count}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
